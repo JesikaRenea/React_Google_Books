4d2266c0a23244cd38275b0fff66b027ec9cfbe8
JesikaRenea <JessicaHayesUCF@outlook.com>
2020-06-03 16:15:42 -0400

    MONGODB_URI update

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const mongoose = require("mongoose");
-const mongoURL = process.env.PROD_MONGODB || "mongodb://localhost:27017/googlebooks"
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks"
+
 mongoose.connect(mongoURL, {useNewUrlParser: true})
   .then(() => {
     console.log("🗄 ==> Successfully connected to mongoDB.");
